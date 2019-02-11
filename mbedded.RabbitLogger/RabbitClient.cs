0d4bf48be53aa871bbe36fd10bc21840c71ab20e
Matthias <nospammb@live.de>
2019-02-11 17:33:04 +0100

    - Connection added for "RabbitClient" class
    - Dummy implementation for Logger

--- mbedded.RabbitLogger/RabbitClient.cs
+++ mbedded.RabbitLogger/RabbitClient.cs
@@ -1,17 +1,37 @@
-﻿namespace mbedded.RabbitLogger {
-  public class RabbitClient {
+﻿using System;
+using RabbitMQ.Client;
 
-    public RabbitClient() {
+namespace mbedded.RabbitLogger {
+
+    /// <summary>
+    ///     This class creates the connection to the rabbit-service and 
+    /// </summary>
+    public class RabbitClient : IDisposable {
+
+        private readonly string _hostName;
+        private IModel _channel;
+
+        public RabbitClient(string xHostName) {
+            _hostName = xHostName;
+        }
+
+        public IModel Channel => _channel;
+
+        public void CreateExchange() {
+            ConnectionFactory factory = new ConnectionFactory() {
+                HostName = _hostName
+            };
+
+            // todo: name for echange via parameter
+            IConnection connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+            _channel.ExchangeDeclare("logs", "fanout", true, false);
+        }
+
+        public void Dispose() {
+            _channel.Dispose();
+        }
+
     }
-
-    //public void aa() {
-    //  var factory = new ConnectionFactory() { HostName = "localhost" };
-    //  var connection = factory.CreateConnection();
-    //  var model = connection.CreateModel();
-
-    //  model.ExchangeDeclare("logs", "fanout", true, false);
-    //}
-
-  }
 
 }
--- mbedded.RabbitLogger/RabbitLogger.cs
+++ mbedded.RabbitLogger/RabbitLogger.cs
@@ -1,14 +1,27 @@
-﻿using mbedded.RabbitLogger.Interfaces;
+﻿using System.Text;
+using mbedded.RabbitLogger.Interfaces;
 
 namespace mbedded.RabbitLogger {
 
   public class RabbitLogger : ILogger {
+    private readonly RabbitClient _client;
+
+    public RabbitLogger(RabbitClient xClient) {
+      _client = xClient;
+    }
 
     public void Dispose() {
       throw new System.NotImplementedException();
     }
 
     public void Debug(string message) {
+
+      byte[] body = Encoding.UTF8.GetBytes(message);
+      
+      // todo: same exchange as rabbit-client
+      _client.Channel.BasicPublish("logs","",
+        false, null, body);
+      
       throw new System.NotImplementedException();
     }
 
